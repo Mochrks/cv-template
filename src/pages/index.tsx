f29c2ac8bd2310e21223cc067f9953ded062df6e
mochrks <mochrizkiks@gmail.com>
2024-11-30 22:37:57 +0700

    feat: copyright

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,44 @@
 "use client";
 
 import LandingPage from "@/components/Home";
-import { Box } from "@mui/material";
+import { Box, Typography, Container, Link } from "@mui/material";
 
 export default function Home() {
   return (
     <Box
-      sx={{ margin: 0, padding: 0, height: '100vh', width: '100%', }}
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+        margin: 0,
+        padding: 0
+      }}
     >
-      <LandingPage />
+      <Box sx={{ flex: 1 }}>
+        <LandingPage />
+      </Box>
+      <Box
+        component="footer"
+        sx={{
+          width: '100%',
+          backgroundColor: 'background.default',
+          py: 2,
+          textAlign: 'center'
+        }}
+      >
+        <Container maxWidth="sm">
+          <Typography variant="body2" color="text.secondary">
+            © {new Date().getFullYear()} All rights reserved by{' '}
+            <Link
+              href="https://github.com/Mochrks"
+              color="primary"
+              underline="hover"
+            >
+              @mochrks
+            </Link>
+          </Typography>
+        </Container>
+      </Box>
     </Box>
   );
-};
+}
